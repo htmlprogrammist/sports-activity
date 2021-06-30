77acfc95e18ec915238b7e22c62091b4e61ea696
Egor Badmaev <badmaeve2511@gmail.com>
2021-06-30 17:20:56 +0300

    init: start of the project 'sports-activity'

---
+++ src/components/rings.jsx
@@ -0,0 +1,69 @@
+import React from "react"
+import styled from "styled-components"
+import { rgba } from "polished"
+import Chart from "react-apexcharts"
+
+const Card = styled.div`
+    width: 300px;
+    border-radius: 3rem;
+    background: #191919;
+`
+
+const CardHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: center;
+    height: 64px;
+`
+
+const ChartWrapper = styled(Chart)`
+    display: flex;
+    justify-content: center;
+`
+
+const CardText = styled.div`
+    font-size: ${props => props.size};
+    font-weight: ${props => (props.bold ? 700: 400)};
+    color: ${props => rgba('white', props.opacity)};
+`
+
+const state = {
+    series: [30, 40, 45],
+    options: {
+        plotOptions: {
+            radialBar: {
+                hollow: { size: "28%"},
+                dataLabels: {
+                    name: { show: false},
+                    value: { show: false}
+                },
+                track: {
+                    background: rgba('white', 0.0375)
+                }
+            }
+        },
+        stroke: { lineCap: 'round'},
+        colors: ['#C200FB', '#2191FB', '#50FFB1']
+    }
+}
+
+const ActivityRings = () => (
+    <Card>
+        <CardHeader>
+            <CardText size="1rem" opacity={0.75}>
+                Activity
+            </CardText>
+            <CardText size="0.75rem" opacity={0.5}>
+                Today
+            </CardText>
+        </CardHeader>
+        <ChartWrapper
+            options={state.options}
+            series={state.series}
+            type="radialBar"
+        />
+    </Card>
+)
+
+export default ActivityRings
